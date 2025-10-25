02a930fb1c601949566f603f27f80940196fd85c
Syed Shifat <shifat1755@gmail.com>
2025-10-26 00:48:38 +0600

    feat(api): added logut

--- app/application/usecases/user_usecase.py
+++ app/application/usecases/user_usecase.py
@@ -6,17 +6,13 @@ from app.domain.errors import (
     EmailAlreadyExistsError,
     UsernameAlreadyExistsError,
     UserNotFoundError,
-    WrongCredentials,
 )
 from app.infrastructure.data.models.user_model import User
-from app.infrastructure.data.redis_refresh_token_client import RedisTokenService
 from app.infrastructure.repositories.user_repo import UserRepository
-from app.infrastructure.security.bcrypt_hasher import hash_password, verify_password
+from app.infrastructure.security.bcrypt_hasher import hash_password
 from app.infrastructure.security.jwt import JWTHandler
 from app.presentation.schemas.user_schema import (
-    Login_data,
     UserCreate,
-    UserCredentials,
     UserUpdate,
 )
 
@@ -24,7 +20,6 @@ from app.presentation.schemas.user_schema import (
 class UserUsecase:
     def __init__(self, db: Session):
         self.repo = UserRepository(db)
-        self.redis_token_service = RedisTokenService()
         self.jwt_handler = JWTHandler()
 
     # Create
@@ -80,40 +75,4 @@ class UserUsecase:
             raise UserNotFoundError
         await self.repo.delete_user(db_user)
         await self.repo.db.commit()
-        return
-
-    async def login(self, user_cred=UserCredentials) -> Login_data:
-        db_user = await self.repo.get_user_by_email(user_cred.email)
-        if not db_user:
-            raise UserNotFoundError
-        if not verify_password(user_cred.password, db_user.hashed_password):
-            raise WrongCredentials
-        accesstoken = self.jwt_handler.generate_access_token(
-            subject=str(db_user.id),
-            extra_claims={
-                "role": str(db_user.role),
-                "user_type": str(db_user.user_type),
-            },
-        )
-        refreshtoken = self.jwt_handler.generate_refresh_token(
-            subject=str(db_user.id),
-            extra_claims={
-                "role": str(db_user.role),
-                "user_type": str(db_user.user_type),
-            },
-        )
-
-        session_id = await self.redis_token_service.store(
-            user_id=str(db_user.id), refresh_token=refreshtoken
-        )
-
-        return Login_data(
-            access_token=accesstoken,
-            refresh_token=refreshtoken,
-            session_id=session_id,
-            user=db_user,
-        )
-
-    async def logout(self, user_id: str, session_id: str) -> None:
-        await self.redis_token_service.revoke(user_id=user_id, session_id=session_id)
         return
--- app/presentation/routes/user_routes.py
+++ app/presentation/routes/user_routes.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter, Depends, HTTPException, Response
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.application.usecases.user_usecase import UserUsecase
@@ -8,17 +8,14 @@ from app.domain.errors import (
     EmailAlreadyExistsError,
     UsernameAlreadyExistsError,
     UserNotFoundError,
-    WrongCredentials,
 )
 from app.infrastructure.data.database import get_db
 from app.presentation.routes.dependencies import get_current_user
 from app.presentation.schemas.user_schema import (
     UserCreate,
-    UserCredentials,
     UserList,
     UserRead,
     UserUpdate,
-    loginresponse,
 )
 
 logger = logging.getLogger(__name__)
@@ -105,63 +102,3 @@ async def delete_user(
         logger.exception("Error deleting user")
         raise HTTPException(status_code=500, detail="Internal server error")
     return
-
-
-@userRouter.post("/login", response_model=loginresponse)
-async def login_user(
-    credential: UserCredentials, response: Response, db: Session = Depends(get_db)
-):
-    usecase = UserUsecase(db=db)
-    try:
-        login_data = await usecase.login(user_cred=credential)
-
-        # Set refresh token in HttpOnly cookie
-        response.set_cookie(
-            key="refresh_token",
-            value=login_data.refresh_token,
-            httponly=True,
-            secure=True,
-            samesite="Strict",
-            path="/auth/refresh",
-        )
-        # Set session_id in HttpOnly cookie
-        response.set_cookie(
-            key="session_id",
-            value=login_data.session_id,
-            httponly=True,
-            secure=True,
-            samesite="Strict",
-            path="/",
-        )
-
-        # Return access token, session_id, user info in JSON
-        return {
-            "access_token": login_data.access_token,
-            "user": login_data.user,
-        }
-    except WrongCredentials:
-        raise HTTPException(status_code=401, detail="Credentials mismatch")
-    except UserNotFoundError:
-        raise HTTPException(status_code=404, detail="User not found")
-    except Exception:
-        logger.exception("Error during login")
-        raise HTTPException(status_code=500, detail="Internal server error")
-
-
-# @userRouter.post("/logout")
-# async def logout_user(
-#     session_id: str = Cookie(None),
-#     db: Session = Depends(get_db),
-#     sender=Depends(get_current_user),
-# ):
-#     if not session_id:
-#         raise HTTPException(status_code=401, detail="No session_id cookie found")
-
-#     usecase = UserUsecase(db)
-#     try:
-#         await usecase.logout(sender["user_id"], session_id)
-#     except Exception:
-#         logger.exception("Error during logout")
-#         raise HTTPException(status_code=500, detail="Internal server error")
-
-#     return {"detail": "Logged out successfully"}
