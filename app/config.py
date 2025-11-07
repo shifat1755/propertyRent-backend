159b07dec5e5047f55db7e96b8bc8f2b6e6f7465
Syed Shifat <shifat1755@gmail.com>
2025-11-07 07:03:14 +0600

    feat(elasticSearch): added docker-compose file to initialize single node elastic cluster

--- app/config.py
+++ app/config.py
@@ -2,7 +2,8 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()  # load .env from root
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 
 class DatabaseConfig:
