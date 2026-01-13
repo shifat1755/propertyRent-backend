4f74dc5f16c8e72ac4c9fa7c82feed7ee80c6843
Syed Shifat <shifat1755@gmail.com>
2026-01-13 21:45:36 +0600

    feat(CORS): added cors setup

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.presentation.routes.auth_routes import authRouter
 from app.presentation.routes.property_routes import propertyRouter
@@ -6,6 +7,17 @@ from app.presentation.routes.user_routes import userRouter
 
 app = FastAPI(debug=True)
 
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(userRouter, prefix="/api", tags=["users"])
 app.include_router(propertyRouter, prefix="/api", tags=["properties"])
